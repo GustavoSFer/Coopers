bbde1cb9147d46ed214a223ac0e38075bb2e68fb
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-25 18:12:19 -0300

    Feat: Criando parte do go to-do list superior

--- src/Pages/PageCoopers.jsx
+++ src/Pages/PageCoopers.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../Components/Button';
 import Menu from '../Components/Menu';
 
 function PageCoopers() {
@@ -9,6 +10,12 @@ function PageCoopers() {
           <Menu />
         </main>
         <div className="escritorio" />
+        <div className="daily">
+          <h1>Organize</h1>
+          <p className="dailly-jobs">your daily jobs</p>
+          <p className="fw-bold">The only way to get things done</p>
+          <Button sty="btn-color">Go to To-do list</Button>
+        </div>
       </div>
     </div>
   );
