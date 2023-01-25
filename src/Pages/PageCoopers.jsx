f8c916191b6712f42f3cc3eaf91a6a0f3f7e4a2c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2023-01-25 19:03:21 -0300

    Feat: Criando a parte de todo-list com a tarje preta

--- src/Pages/PageCoopers.jsx
+++ src/Pages/PageCoopers.jsx
@@ -17,6 +17,19 @@ function PageCoopers() {
           <Button sty="btn-color">Go to To-do list</Button>
         </div>
       </div>
+      <div className="skew">
+        <div>
+          <h2>To-do List</h2>
+          <div className="width-list">
+            <p>
+              Drag and drop to set your main priorities, check
+            </p>
+            <p>
+              when done and create what´s new.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
